470e53149dcc05dfbcce347a9bdac0c643d8bcd1
ZeroK <149679147+SSJ-05@users.noreply.github.com>
2026-04-12 12:48:09 +0530

    Update malloc_impl.hpp

--- malloc_impl.hpp
+++ malloc_impl.hpp
@@ -1,5 +1,11 @@
 // malloc package implementation// 12.04.26// ZeroK
 
+/*
+NOTEs: need error handling free func that returns explicit error messages when things go wrong
+no undefined behavior on negative increment
+padding alignment to 64 bit
+*/
+
 #pragma once
 #include <cstdlib>
 #include <cstddef>
