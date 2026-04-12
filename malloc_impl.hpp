baba38e71e590a3e4bd8a27b57b02962b164414f
ZeroK <149679147+SSJ-05@users.noreply.github.com>
2026-04-12 12:36:02 +0530

    Add files via upload

---
+++ malloc_impl.hpp
@@ -0,0 +1,13 @@
+// malloc package implementation// 12.04.26// ZeroK
+
+#pragma once
+#include <cstdlib>
+#include <cstddef>
+#include <cstdint>
+#include <sys/mman.h>
+#include <unistd.h>
+
+namespace zerok {
+
+
+} // namespace zerok
